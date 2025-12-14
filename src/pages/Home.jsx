669be6e6053ad3a65db4f52b862143e73ad997c7
Abhijeet Rao <abhijeetrao7800@gmail.com>
2025-12-14 21:21:15 +0530

    added proxy server by vite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import axios from 'axios'
 
 function Home() {
-  const fetchData = async()=>{
-   const response = await axios.get("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.5106395&lng=83.7849277&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-   console.log(response);
+  const fetchRestaurants = async () => {
+  try {
+    
+    const response = await axios.get(
+      '/swiggy/dapi/restaurants/list/v5?lat=26.5106395&lng=83.7849277&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    );
+    console.log(response);
+  } catch (error) {
+    console.error("Error fetching Swiggy data:", error);
   }
+}
 
-  fetchData();
+  fetchRestaurants();
   return (
     <div>Home</div>
   )
